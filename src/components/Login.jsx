82eaf2183d3865f97333ee6bfe86f7c63ad1c575
antho1810 <isidroromero.1810@gmail.com>
2023-01-06 17:23:43 -0500

    first commit

---
+++ src/components/Login.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import loginImg from '../assets/OIP.jpg'
+
+export const Login = () => {
+  return (
+    <div className='grid grid-cols-1 sm:grid-cols-2 h-screen w-full'>
+      <div className='hidden sm:block'>
+      <img className='w-full h-full object-cover' src={loginImg} alt="" />
+      </div>
+      <div className='bg-gray-400 flex flex-col justify-center'>
+        <form className='kax-w-[400px] w-full mx-auto bg-gray-900 p-8 px-8 rounded-lg' action="">
+          <h2 className='text-4x1 dark:text-white font-bold text-center'>Sign in</h2>
+          <div className='flex flex-col text-gray-400 py-2'>
+            <label htmlFor="">User Name</label>
+            <input className='rounded-lg bg-gray-700 mt-2 p-2 focus:border-blue-500 focus:bg-gray-800 focus:outline-none' type="text" />
+          </div>
+          <div className='flex flex-col text-gray-400 py-2'>
+            <label htmlFor="">Password</label>
+            <input className='rounded-lg bg-gray-700 mt-2 p-2 focus:border-blue-500 focus:bg-gray-800 focus:outline-none' type="text" />
+          </div>
+          <div className='flex justify-between text-gray-400 py-2'>
+            <p className='flex items-center'><input className='mr-2' type="checkbox" />Remember Me</p>
+          <p>Forgot password? </p></div>
+          <button className='w-full my-5 py-2 bg-teal-500 shadow-lg shadow-teal-500/50 hover:shadow-teal-500/40 text-white font-semibold rounded-lg '>Sign In</button>
+        </form>
+      </div>
+    </div>
+  )
+}
